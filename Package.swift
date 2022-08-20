b4daadf1de1897caf24820d477454f31fae59d80
TA MINH QUANG <quang@TAs-Mac-mini.local>
2022-08-20 19:36:45 +0700

    Update

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ import PackageDescription
 
 
 let package = Package(
-    name: "SwiftUITabs",
+    name: "SwiftUI-Tabs",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SwiftUITabs",
-            targets: ["SwiftUITabs"]),
+            name: "SwiftUI-Tabs",
+            targets: ["SwiftUI-Tabs"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,7 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftUITabs",
+            name: "SwiftUI-Tabs",
             dependencies: [],
             path: "Sources"),
     ]
