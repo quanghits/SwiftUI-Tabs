5b25f7426383623c988a9c7d3c3f3cdea0277b6c
TA MINH QUANG <quang@TAs-Mac-mini.local>
2022-08-20 19:49:17 +0700

    Update

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUITabs",
-    platforms: [
-        .iOS(.v14)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
